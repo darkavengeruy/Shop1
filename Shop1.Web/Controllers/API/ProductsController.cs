cbf71e11b16dea235b96602292f256fbed396a66
FERNANDO FRANCO <ferfranco.uy@hotmail.com>
2019-05-10 02:35:48 -0400

    API Products Controller

---
+++ Shop1.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Shop1.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository )
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
